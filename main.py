19c846ceb699064a06440b3335ace91f11a3955f
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2025-04-01 09:47:00 +0000

    break

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ slack_token = os.environ["SLACK_TOKEN"]
 pagerduty_client = APISession(pagerduty_token)
 slack_client = WebClient(token=slack_token)
 
-
 def get_on_call_schedule_name():
     """
     Fetches the name of the on-call schedule from PagerDuty.
@@ -36,7 +35,6 @@ def get_on_call_schedule_name():
         schedule_name = response.json()["schedule"]["name"]
 
     return schedule_name
-
 
 def get_on_call_user():
     """
